4fc795cfa784aa9eb6eab62e95386dd7ffc94510
Filipy <filipytav17@gmail.com>
2026-05-10 13:26:31 -0300

    Adds MusicLibrary

---
+++ src/structs/Library.py
@@ -0,0 +1,54 @@
+from music import Music
+from structs.MusicNode import SMusicNode
+
+
+class MusicLibrary:
+    def __init__(self):
+        self.__head: SMusicNode | None = None
+        self.__tail: SMusicNode | None = None
+        self.__count: int = 0
+
+    def insert_at(self, m: Music, pos: int) -> bool:
+        if pos < 0 or pos > self.__count:
+            print(f"Index out of range: {pos}, the list has {self.__count} element(s)")
+            return False
+
+        new_node: SMusicNode = SMusicNode(m)
+
+        # Empty
+        if self.is_empty():
+            self.__head = self.__tail = new_node
+            self.__count += 1
+            return True
+
+        # new_node is now the head
+        if pos == 0:
+            new_node.next = self.__head
+
+            self.__head = new_node
+        # new_node is now the tail
+        elif pos == self.__count:
+            assert self.__tail
+            self.__tail.next = new_node
+
+            self.__tail = new_node
+        else:
+            current: SMusicNode | None = self.__head
+
+            for _ in range(pos - 1):
+                if not current:
+                    return False
+                current = current.next
+
+            # Because LSP
+            assert current is not None
+            assert current.next is not None
+
+            new_node.next = current.next
+            current.next = new_node
+
+        self.__count += 1
+        return True
+
+    def is_empty(self) -> bool:
+        return not (self.__head and self.__tail)
