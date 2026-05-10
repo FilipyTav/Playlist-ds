112a5498220f31639c146d4fe8468b67c375c0e1
Filipy <filipytav17@gmail.com>
2026-05-10 12:57:44 -0300

    Creates Music class

---
+++ src/music.py
@@ -0,0 +1,7 @@
+class Music:
+    def __init__(self, id, title, artist, genre, bpm) -> None:
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.type = genre
+        self.bpm = bpm
