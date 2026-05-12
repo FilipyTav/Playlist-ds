c7595fa979bcd047de51e1f5320232596e4a0200
Filipy <filipytav17@gmail.com>
2026-05-11 22:43:16 -0300

    Now can remove song

--- src/ui.py
+++ src/ui.py
@@ -147,19 +147,30 @@ def lib_remove_song(library: MusicLibrary) -> Screen:
         except ValueError:
             return False
 
-    id_str: str = get_and_validate_input(
-        "ID(B para cancelar)",
-        validator=validate_id,
-        error_msg="ID deve ser um número positivo",
-    )
+    rm_song: Music | None = None
+    while not rm_song:
+        id_str: str = get_and_validate_input(
+            "ID(B para cancelar)",
+            validator=validate_id,
+            error_msg="ID deve ser um número positivo",
+        )
 
-    if id_str == "b":
-        return Screen.BACK
+        if id_str == "b":
+            return Screen.BACK
 
-    id: int = int(id_str)
+        id: int = int(id_str)
 
-    # print("-" * 40)
-    # print(f"\033[92mMúsica '{title}' removida com sucesso!\033[0m")
+        rm_song = library.find_by_id(id)
+
+        if not rm_song:
+            print(f"ID ({id}) não existe na biblioteca. Tente novamente.\n")
+            continue
+
+    if library.remove_by_id(rm_song.id):
+        print("-" * 40)
+        print(f"\033[92mMúsica '{rm_song.title}' removida com sucesso!\033[0m")
+    else:
+        print(f"Algo deu errado. Não foi possível remover {rm_song.title}")
 
     choice = input(NAV_PROMPT).strip().lower()
     nav = handle_global_nav(choice)
